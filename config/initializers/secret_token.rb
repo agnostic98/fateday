69b76517944200f6ae73a5df2e3fb4a598539817
Peter Chang <peter@socialrithmic.com>
2013-08-03 14:02:39 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fateday::Application.config.secret_token = '12d539a3aa41655bbadac61fcb52cb59bed026345fdad37bd023ae41a423504b2b80cce2028594050e11c1514cf89162cd0f561c6d31733551c44ae252d4a6b5'
